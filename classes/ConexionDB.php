f5bc3a0cbbe5659d11af7d84d0d68580cc6f47a0
camilo <ancal999@gmail.com>
2015-02-05 23:09:02 -0500

    se agrega manejador DB y logica para registro de usuario

---
+++ classes/ConexionDB.php
@@ -0,0 +1,52 @@
+<?php
+
+class ConexionDB
+{
+  	//Variables para configurar la conexion a la BD.
+	var $host;
+	var $username;
+	var $password;
+	var $db_name;
+	var $conexion;
+
+	//Constructor.
+	function ConexionDB()
+	{
+		$this->host="127.0.0.1"; // Host name 
+		$this->username="root"; // Mysql username 
+		$this->password=""; // Mysql password 
+		$this->db_name="cauac"; // Database name 
+	}
+
+	//Funcion: Realiza la conexion con la base de datos y activa variable de conexion.
+	function conectarDB()
+	{
+		if(!is_null($this->conexion))
+			return true;
+
+		// Connect to server and select databse.
+		$this->conexion = mysql_connect($this->host, $this->username, $this->password)or die("cannot connect"); 
+		mysql_select_db($this->db_name) or die("cannot select DB");
+
+		return $this->conexion;
+	}
+
+	//Funcion: Obtener conexion.
+    function getConexionDB(){
+    	return $this->conexion; 
+    }
+
+    //Funcion: Ejecuta una consulta enviada por parametro.
+    function ejecutarConsultaDB($cadenaSQL)
+    {
+    	//$result = odbc_exec($this->conn, $cadenaSQL);
+    	//return $result;
+    }
+
+    //Funcion: Desconecta en enlace con la base de datos.
+    function desconectarDB()
+    {
+		mysql_close($this->conexion);
+    }
+}
+?>
--- classes/registrar.php
+++ classes/registrar.php
@@ -1,3 +1,34 @@
 <?php
-	echo "hola mundo";
+	require_once('conexionDB.php');
+	//error_reporting(E_ALL);
+
+	$conn = new ConexionDB;
+	$conn->conectarDB();
+
+	$cedula = $_POST["cedula"];
+	$nombres = $_POST["name"];
+	$apellidos = $_POST["lastName"];
+	$usuario = $_POST["user"];
+	$clave = $_POST["pass"];
+	$empresa = $_POST["bussines"];
+
+	$sql = "SELECT usuario FROM usuarios WHERE usuario = '$usuario'";
+	$retval = mysql_query( $sql, $conn->getConexionDB() );
+	$row = mysql_fetch_assoc($retval);
+
+	//validacion que no se cree el mismo loggin de usuario.
+	if(mysql_num_rows($retval) > 0)
+	{
+		echo "El nombre de usuario ya existe. Seleccionar otro" ;
+	}
+	else
+	{
+		$sql = "INSERT INTO  usuarios (cedula, nombres, apellidos, usuario, clave, rol, empresa)" .
+					"VALUES ($cedula, '$nombres', '$apellidos', '$usuario', '$clave', 0, '$empresa');";
+		$retval = mysql_query( $sql, $conn->getConexionDB() );
+		
+		echo "Registro satisfactorio!";
+	}
+
+	$conn->desconectarDB();
 ?>   

68d290eff7022c583bb9d064b7d7fe0c238ef799
Ricardo <horlock69@gmail.com>
2015-02-12 22:12:25 -0500

    no message

--- redactar.php
+++ redactar.php
@@ -2,9 +2,11 @@
 <ul class="redactMenu nav nav-pills ">  
   <li role="presentation"><a href="#">Enviar</a></li>
   <li role="presentation"><a href="#">Limpiar</a></li>
-  <li role="presentation" class=""><select class="form-control nav-right" required="required" placeholder="hoa que">
+  <li role="presentation">
+  <label for="selectUser">Enviar a:</label>
+  <select class="form-control" id="selectUser" required="required">
   <optgroup label="Usuarios Registrados">
-  <option value="User 1" disabled="">Enviar a:</option>
+  <option value="User 1">User 1</option>
   <option value="User 2">User 2</option>
   <option value="User 3">User 3</option>
   <option value="User 4">User 4</option>

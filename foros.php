0e01bd916894304404ac50aa149649041e63e8d6
Ricardo <horlock69@gmail.com>
2015-02-12 20:27:37 -0500

    cambio estructura foros.php

--- foros.php
+++ foros.php
@@ -1,8 +1,25 @@
 <h2 class="orange">Foros</h2>
+<ul class="nav nav-pills ">
+<li role="presentation"><a href="#">Crear Foro</a></li> 
+</ul>
 
 <ul>
-<li><h3><a href="consultoria.php">Consultoria</a></h3></li>
-<li><h3><a>Recursos Humanos</a></h3></li>
-<li><h3><a>Foro3</a></h3></li>
-<li><h3><a>Foro4</a></h3></li>
+<?php 
+		include("classes/seguridad.php");
+		require_once('classes/conexionDB.php');
+		//error_reporting(E_ALL);
+
+		$conn = new ConexionDB;
+		$conn->conectarDB();
+
+		$cedula = $_SESSION["cedula"];
+
+		$sql = "SELECT * FROM mensajes WHERE receptor = $cedula";
+		$retval = mysql_query( $sql, $conn->getConexionDB() );
+		
+		while($row = mysql_fetch_assoc($retval))
+		{
+			echo "<li><h3><a onClick='redirectMensaje(" . $row['codigo'] .  ")'>" . $row['titulo'] . "</a><a class='navbar-right orange'>Editar</a><a class='navbar-right orange'>Eliminar&nbsp;&nbsp;</a><a class='navbar-right'>Ver Mas&nbsp;&nbsp;</a></h3></li>";
+		}
+	?>
 </ul>

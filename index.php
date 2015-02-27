4a9903295bb253e53e156c4f95f10e234a87782e
Ricardo <horlock69@gmail.com>
2015-02-26 23:38:15 -0500

    no message

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             <?php
             if(isset($_GET['cerrar']))
             {
-                ob_start();
+                session_start();
                 session_destroy();
                 echo"<script language='javascript'>window.location='index.php'</script>;";
                 // header("location: index.php");
